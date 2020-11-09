a7604bd0d229e835b0144cc6f7febb5a69d60e60
ralgond <ht201806@outlook.com>
2020-11-09 16:49:55 +0800

    Add xml based config for provider.

---
+++ dubbo-example-provider/src/main/java/com/github/ralgond/de/provider/ApiProvider2.java
@@ -0,0 +1,14 @@
+package com.github.ralgond.de.provider;
+
+import java.io.IOException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApiProvider2 {
+
+	public static void main(String args[]) throws IOException {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("provider.xml");
+        context.start();
+        System.in.read(); // 按任意键退出
+	}
+}
