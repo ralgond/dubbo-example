959e0217ebf1a84f9a5a8975ac0fe3befd702c58
ralgond <ht201806@outlook.com>
2020-11-09 13:31:36 +0800

    Add xml config based consumer.

---
+++ dubbo-example-consumer/src/main/java/com/github/ralgond/de/consumer/ApiConsumer2.java
@@ -0,0 +1,19 @@
+package com.github.ralgond.de.consumer;
+
+import org.apache.dubbo.rpc.RpcContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.github.ralgond.de.sdk.GreetingService;
+
+public class ApiConsumer2 {
+	public static void main(String args[]) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
+		context.start();
+		
+		GreetingService service = (GreetingService)context.getBean("greetingService");
+		
+		RpcContext.getContext().setAttachment("company", "raglond");
+		
+		System.out.println(service.sayHello("world"));
+	}
+}
