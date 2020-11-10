fa56e1eb77da3676f5fdb3cc48676b0e36d575f6
ralgond <ht201806@outlook.com>
2020-11-10 11:53:55 +0800

    lab-executes

--- dubbo-example-provider/src/main/java/com/github/ralgond/de/provider/ApiProvider2.java
+++ dubbo-example-provider/src/main/java/com/github/ralgond/de/provider/ApiProvider2.java
@@ -9,6 +9,7 @@ public class ApiProvider2 {
 	public static void main(String args[]) throws IOException {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("provider.xml");
         context.start();
+        System.out.println("server started.");
         System.in.read(); // 按任意键退出
 	}
 }
